fd30e6457d6f4819b8914bd4d5afa2aca0e97800
Sergio Peña <sergioeduardo2000@gmail.com>
2024-03-21 00:33:40 -0500

    Adding export to csv and json functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :investments, only: [:new, :create]
+
+  resources :cryptocurrencies do
+    collection do
+      get 'export_csv'
+      get 'export_json'
+    end
+  end
   root "cryptocurrencies#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
